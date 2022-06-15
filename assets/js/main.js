9114509163786f204282d8f8223565d6c98a3b62
ThalizFajardo <fajardothaliz@gmail.com>
2022-06-15 00:35:15 -0500

    dark-theme agregado && eliminacion de estilados innecesarios de css

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,15 +5,15 @@ let cartOverlay = document.querySelector(".shopping-cart-overlay")
 let cartClose = document.getElementById("cart-close")
 
 
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", () => {
     mostrarProductos()
 })
 
-cartIcon.addEventListener( "click", () =>{
+cartIcon.addEventListener("click", () => {
     cartOverlay.classList.add("mostrar")
 })
 
-cartClose.addEventListener( "click", () =>{
+cartClose.addEventListener("click", () => {
     cartOverlay.classList.remove("mostrar")
 })
 
@@ -23,10 +23,19 @@ cartClose.addEventListener( "click", () =>{
 
 let header = document.querySelector("header")
 
-window.addEventListener( "scroll", () =>{
-    if( window.scrollY > 60 ){
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 60) {
         header.classList.add("scroll-header")
-    }else{
+    } else {
         header.classList.remove("scroll-header")
     }
+})
+
+/*cambiando a dark-mode */
+let themeIcon = document.getElementById("theme-toggler")
+
+let body = document.querySelector("body")
+
+themeIcon.addEventListener("click", (e) => {
+    body.classList.toggle("dark-theme")
 })
